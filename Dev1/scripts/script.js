07508427f97c866d158399abcdfca769a353aa2c
Katie Bumatay <bumatay.katie@gmail.com>
2019-02-21 14:37:38 -0600

    image and phrase placeholder (bugs)

--- Dev1/scripts/script.js
+++ Dev1/scripts/script.js
@@ -95,44 +95,30 @@
 $(document).ready(function() {
 
 
-    $("h1").mouseover(function(){
+    $("h1").hover(function(){
         $(this).toggleClass("blurtext");
     });
 
-    $("h1").mouseout(function(){
-        $(this).toggleClass("blurtext");
-    });
-
-    $(".sectionHeader").mouseover(function(){
+    $(".sectionHeader").hover(function(){
         $(this).toggleClass("blurtext2");
     });
 
-    $(".sectionHeader").mouseout(function(){
+    $(".pledge1, .pledge2, .pledge3, .pledge4, .pledge5, .pledge6, .pledge7, .pledge8").hover(function(){
         $(this).toggleClass("blurtext2");
     });
 
-    $(".pledge1, .pledge2, .pledge3, .pledge4, .pledge5, .pledge6, .pledge7, .pledge8").mouseover(function(){
+    $(".dec1, .dec2, .dec3, .dec4, .dec5, .dec6, .dec7, .dec8, .dec9, .dec10, .dec11, .dec12, .dec13").hover(function(){
         $(this).toggleClass("blurtext2");
     });
 
-    $(".pledge1, .pledge2, .pledge3, .pledge4, .pledge5, .pledge6, .pledge7, .pledge8").mouseout(function(){
+    $(".const1, .const2, .const3, .const4, .const5, .const6, .const7, .const8").hover(function(){
         $(this).toggleClass("blurtext2");
     });
 
-    $(".dec1, .dec2, .dec3, .dec4, .dec5, .dec6, .dec7, .dec8, .dec9, .dec10, .dec11, .dec12, .dec13, .dec14").mouseover(function(){
-        $(this).toggleClass("blurtext2");
-    });
+    /********* make variables **********/
 
-    $(".dec1, .dec2, .dec3, .dec4, .dec5, .dec6, .dec7, .dec8, .dec9, .dec10, .dec11, .dec12, .dec13, .dec14").mouseout(function(){
-        $(this).toggleClass("blurtext2");
-    });
-
-    $(".const1, .const2, .const3, .const4, .const5, .const6, .const7, .const8").mouseover(function(){
-        $(this).toggleClass("blurtext2");
-    });
-
-    $(".const1, .const2, .const3, .const4, .const5, .const6, .const7, .const8").mouseout(function(){
-        $(this).toggleClass("blurtext2");
+    $(".phrase").click(function(){
+        $(".imgContainer, .phraseHeadingContainer").toggleClass("show");
     });
 
 
