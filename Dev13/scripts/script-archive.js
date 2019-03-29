6c9b53c729efee762ff864991e2acd4acb76ece1
Katie Bumatay <bumatay.katie@gmail.com>
2019-03-28 20:14:15 -0500

    work on archive

--- Dev13/scripts/script-archive.js
+++ Dev13/scripts/script-archive.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 
+    $(".goToPledge").click(function() {
+        // $('html,body').animate({
+        // scrollTop: $("#pledge").offset().top},
+        // 'slow');
+
+        $('html,body').animate({
+        scrollTop: $("#pledge").offset().top}, 'slow');
+
+    });
+
+    $(".goToDec").click(function() {
+        $('html,body').animate({
+        scrollTop: $("#declaration").offset().top}, 'slow');
+
+    });
+
+    $("li").click(function() {
+        // console.log("clicked list item");
+        $(".list-container").toggleClass("showArticle");
+
+    });
+
 });
